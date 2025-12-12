0a37fbf5e48c996dcc695b2247d5f4cde078d558
L-Whone <bibo06@icloud.com>
2025-12-11 18:24:56 -0800

    MODIFIED: TIME OF UPDATE
    Time

--- Pacman/Pacman/source.cpp
+++ Pacman/Pacman/source.cpp
@@ -10,20 +10,26 @@ int main()
     LOG_LN("Press arrow keys (UP, DOWN, LEFT, RIGHT). Press 'Q' to quit.\n");
 
     bool running = true;
-
+    int time = 0;
     while (running)
     {
-        inputMgr.Update();
+        if (time % 40000000 == 0)
+        {
+            inputMgr.Update();
 
-        const InputState& state = inputMgr.GetState();
+            const InputState& state = inputMgr.GetState();
 
-        if (state.IsUp())    std::cout << "UP\n";
-        else if (state.IsDown())  std::cout << "DOWN\n";
-        else if (state.IsLeft())  std::cout << "LEFT\n";
-        else if (state.IsRight()) std::cout << "RIGHT\n";
+            if (state.IsUp())    LOG_LN("UP\n");
+            else if (state.IsDown())  LOG_LN("DOWN\n");
+            else if (state.IsLeft())  LOG_LN("LEFT\n");
+            else if (state.IsRight()) LOG_LN("RIGHT\n");
 
-        if (GetAsyncKeyState('Q') & 0x8000)
-            running = false;
+            if (GetAsyncKeyState('Q') & 0x8000)
+                running = false;
+
+        }
+
+        time++;
     }
 
     LOG_LN("Exiting...\n");
