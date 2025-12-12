73b7f23fa4da24376e62116bd0092dbfec02d7de
pg29ken <pg29ken@vfs.com>
2025-12-11 18:49:04 -0800

    ADDED Pacman
    Functions to be implemented -> Needs Game System

--- Pacman/Pacman/Entity.hpp
+++ Pacman/Pacman/Entity.hpp
@@ -4,24 +4,19 @@
 #include "Utils.hpp"
 #include "Object.hpp"
 
-class Entity : Object
+class Entity : public Object
 {
 public:
 	Entity(std::string Name, std::pair<int,int> Position) : Object(Name, Position),  _isAlive(true) {}
 	Entity(std::string Name) : Object(Name), _isAlive(true) {}
+	Entity() : _isAlive(true) {}
 
 	void Move();
 	virtual void Controller() {};
 	virtual void OnDeath() {}; // on health = 0 -> different for ghosts and player
 
 protected:
-	enum MoveDirection
-	{
-		UP,
-		RIGHT,
-		LEFT,
-		DOWN
-	};
+	
 	MoveDirection _moveDirection = UP;
 	bool _isAlive;
 
