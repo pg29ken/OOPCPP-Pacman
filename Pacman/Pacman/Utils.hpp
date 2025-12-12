00fa0b54ff3acbdefa37d4ca80b452631771e502
pg29ken <pg29ken@vfs.com>
2025-12-11 17:41:34 -0800

    INIT COMMIT

---
+++ Pacman/Pacman/Utils.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <format>
+
+#define LOG(X) std::cout << X
+#define LOG_LN(X) std::cout << X << std::endl
+#define LOG_EMPTY_LN() LOG("")
---
+++ Pacman/Pacman/source.cpp
@@ -0,0 +1,6 @@
+#include "Utils.hpp"
+
+int main()
+{
+
+}
