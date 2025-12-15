8e227a08f516011826f26704a58bd8f28dfc61e8
pg29ken <pg29ken@vfs.com>
2025-12-14 19:54:49 -0800

    ADDED GameManager -> Menus ADDED Escape + Enter in GameManager
    Interactive Menus
    
    To be done: Bind Menu opens to Escape

---
+++ Pacman/Pacman/GameManager.hpp
@@ -0,0 +1,40 @@
+// Copyright @ Ken Arigo - VFS 2025-2026
+
+#pragma once
+#include "Utils.hpp"
+
+class GameManager
+{
+	// Holds reference to systems
+	
+public:
+	static void StartMenu();
+	static void PauseGame();
+	static void WinScreen();
+
+	static void EndGame();
+
+	static GameManager* GetInstance();
+
+private:
+	static GameManager* _sInstance;
+};
+
+
+class MenuGraphics
+{
+	friend class GameManager;
+
+	// Pause Menus
+
+private:
+
+	static void StartMenuStart();
+	static void StartMenuExit();
+
+	static void PauseContinue();
+	static void PauseRestart();
+	static void PauseExit();
+
+
+};
